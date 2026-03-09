92f1269d1f3af595503ebba44b9608c1eb96c900
Raquel <106412297+raquelarias123@users.noreply.github.com>
2026-03-09 01:13:00 -0700

    Update Celebrity.java

--- Celebrity.java
+++ Celebrity.java
@@ -1,1 +1,28 @@
+public class Celebrity
+{
+    private String answer;
+    private String clue;
+
+    public Celebrity(String answer, String clue)
+    {
+        this.answer = answer;
+        this.clue = clue;
+    }
+
+    public String getAnswer()
+    {
+        return answer;
+    }
+
+    public String getClue()
+    {
+        return clue;
+    }
+
+    public String toString()
+    {
+        return "Celebrity: " + answer + " Clue: " + clue;
+    }
+}
+
 
