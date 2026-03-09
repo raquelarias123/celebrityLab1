16a8aa36702e8d91868b8ddafee3f8cd1c2ae360
Raquel <106412297+raquelarias123@users.noreply.github.com>
2026-03-09 01:17:03 -0700

    Update CelebrityFrame.java

--- CelebrityFrame.java
+++ CelebrityFrame.java
@@ -1,1 +1,22 @@
+import javax.swing.JFrame;
 
+public class CelebrityFrame extends JFrame
+{
+    private CelebrityGame baseGame;
+
+    public CelebrityFrame(CelebrityGame game)
+    {
+        baseGame = game;
+
+        setTitle("Celebrity Game");
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
+    public void replaceScreen(String screen)
+    {
+        System.out.println("Switching to screen: " + screen);
+    }
+}
