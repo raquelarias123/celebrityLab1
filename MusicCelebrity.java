319b0cbd690c208697365bfff72ca650c8d881dd
Raquel <106412297+raquelarias123@users.noreply.github.com>
2026-03-09 01:14:15 -0700

    Update MusicCelebrity.java

--- MusicCelebrity.java
+++ MusicCelebrity.java
@@ -1,1 +1,28 @@
 
+public class MusicCelebrity extends Celebrity
+{
+    private String genre;
+
+    public MusicCelebrity(String answer, String clue, String genre)
+    {
+        super(answer, clue);
+        this.genre = genre;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    @Override
+    public String getClue()
+    {
+        return super.getClue() + " (Genre: " + genre + ")";
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Music Celebrity: " + getAnswer() + " Genre: " + genre;
+    }
+}
