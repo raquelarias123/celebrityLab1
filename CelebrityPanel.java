109d573a2e3de0a782c54e75b0bcefd75598abd6
Raquel <106412297+raquelarias123@users.noreply.github.com>
2026-03-09 01:18:04 -0700

    Update CelebrityPanel.java

--- CelebrityPanel.java
+++ CelebrityPanel.java
@@ -1,1 +1,11 @@
+import javax.swing.JPanel;
 
+public class CelebrityPanel extends JPanel
+{
+    private CelebrityGame baseGame;
+
+    public CelebrityPanel(CelebrityGame game)
+    {
+        baseGame = game;
+    }
+}
