7b8bc77cde1d3b127b6569767c75ecb4b3a22966
Raquel <106412297+raquelarias123@users.noreply.github.com>
2026-03-09 01:17:40 -0700

    Update StartPanel.java

--- StartPanel.java
+++ StartPanel.java
@@ -1,1 +1,11 @@
+import javax.swing.JPanel;
 
+public class StartPanel extends JPanel
+{
+    private CelebrityGame baseGame;
+
+    public StartPanel(CelebrityGame game)
+    {
+        baseGame = game;
+    }
+}
